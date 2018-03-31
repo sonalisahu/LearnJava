976c0cd71b2f0c412a88cc98395af3819f47c41c
RamanSahu <ramansahu@gmail.com>
2018-03-31 13:26:31 +0530

    added SingleLinkedlist

--- src/Core/Hello.java
+++ src/Core/Hello.java
@@ -7,9 +7,4 @@ public class Hello {
 	}
 
 }
-/*
-To study from - 
-http://www.java2novice.com/java-interview-programs/ 
-http://javarevisited.blogspot.in/2014/07/top-50-java-multithreading-interview-questions-answers.html#axzz5BGNO5ELd
 
-*/
