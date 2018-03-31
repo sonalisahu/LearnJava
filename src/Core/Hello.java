1a986d7154f1443f84866cd10d7c1b76fcf482e6
RamanSahu <ramansahu@gmail.com>
2018-03-31 13:13:28 +0530

    added1

---
+++ src/Core/Hello.java
@@ -0,0 +1,15 @@
+package Core;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world !!");		
+	}
+
+}
+/*
+To study from - 
+http://www.java2novice.com/java-interview-programs/ 
+http://javarevisited.blogspot.in/2014/07/top-50-java-multithreading-interview-questions-answers.html#axzz5BGNO5ELd
+
+*/
